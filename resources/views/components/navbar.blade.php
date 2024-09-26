5e227bb0890f83428dddc67f35ea3c32e90f929d
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-09-26 10:38:11 -0300

    save

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-primary p-4 shadow-md fixed top-0 w-full z-50">
     <div class="container mx-auto flex justify-between items-center">
         <h1 class="text-2xl font-bold text-secondary"><a href="{{ route('home') }}">deNexos</a></h1>
-        <div>
+        <div class="space-x-4">
             @if (Auth::check())
                 <a href="{{ route('dashboard') }}"
                     class="bg-secondary text-black px-4 py-2 hover:bg-lightGray rounded hover:cursor-pointer">Dashboard</a>
