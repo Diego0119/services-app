993e1b4e3f526d2a2b08e8e7b62261b677f0c84a
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-10-25 16:48:29 -0300

    save

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -10,21 +10,21 @@
         <!-- Producto/Servicio 1 -->
         @if (count($notices) > 0)
             @foreach ($notices as $notice)
-                <div class="flex-shrink-0 w-62 bg-gray-100 p-4 rounded-lg shadow-md text-center">
-                    <img src="https://via.placeholder.com/300" alt="Producto 1"
-                        class="w-full h-48 object-cover rounded-md mb-4">
-                    <h3 class="text-lg font-bold mb-2">{{ $notice->title }}</h3>
-                    <p class="text-gray-700 mb-2"></p>
-                    <p class="text-green-500 font-bold mb-2">{{ $notice->price }} CLP</p>
-                    <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"><a
-                            href="{{ route('notice.show', $notice->id) }}">Ver</a></button>
-                </div>
+                @if (!$notice->status == ('EN_REVISIÓN' || 'PAUSADO'))
+                    <div class="flex-shrink-0 w-62 bg-gray-100 p-4 rounded-lg shadow-md text-center">
+                        <img src="https://via.placeholder.com/300" alt="Producto 1"
+                            class="w-full h-48 object-cover rounded-md mb-4">
+                        <h3 class="text-lg font-bold mb-2">{{ $notice->title }}</h3>
+                        <p class="text-gray-700 mb-2"></p>
+                        <p class="text-green-500 font-bold mb-2">{{ $notice->price }} CLP</p>
+                        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"><a
+                                href="{{ route('notice.show', $notice->id) }}">Ver</a></button>
+                    </div>
+                @endif
             @endforeach
         @else
             <div class="flex-shrink-0 w-66 p-4 rounded-lg text-center">
-
                 <p class="text-gray-700 mb-2">No hay posts</p>
-
             </div>
         @endif
 </x-app-layout>
