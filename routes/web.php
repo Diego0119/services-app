84d32e32e3f5081a76328793aa79c8357bbd5f12
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-12-17 11:24:30 -0300

    corrigiendo error

--- routes/web.php
+++ routes/web.php
@@ -14,48 +14,48 @@ use App\Http\Controllers\UploadNoticeController;
 use App\Http\Middleware\CheckIfNoticeIsHighlighted;
 use App\Http\Middleware\CheckIfUserIsAdmin;
 
-
-
-
-Route::get('/', [WelcomeController::class, 'index'])->name('home');
-
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('/details', [DetailsController::class, 'index'])->name('details.index');
-Route::get('/account', [AccountController::class, 'index'])->name('account.index');
-Route::put('/account', [AccountController::class, 'modifyUserInformation'])->name('account.post');
-Route::get('/upload-notice', [NoticeController::class, 'uploadNotice'])->name('upload-notice.index');
-Route::post('/upload-notice', [NoticeController::class, 'store'])->name('upload-notice.store');
-Route::get('/my-notices', [MyNoticesController::class, 'index'])->name('my-notices.index');
-Route::get('/notices', [NoticeController::class, 'getAllNotices'])->middleware(CheckIfUserIsAdmin::class)->name('get-all-notices');
-Route::get('get-communes/{region_id}', [CommunesController::class, 'getCommunes']);
-Route::get('/category/{id}', [CategoryController::class, 'showCategory'])->name('category.show');
-Route::get('/notice/{id}', [NoticeController::class, 'showNotice'])->name('notice.show');
-Route::get('/notice-modify/{id}', [NoticeController::class, 'modifyNotice'])->name('notice.modify');
-Route::post('/notice-modify/{id}', [NoticeController::class, 'storeModifiedNotice'])->name('notice.update');
+// Rutas publicas
+Route::get('/', [WelcomeController::class, 'index'])->name('home'); // Home page
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index'); // List of categories
+Route::get('/details', [DetailsController::class, 'index'])->name('details.index'); // Details page
 Route::get('/contact', function () {
     return view('contact.index');
-})->name('contact');
+})->name('contact'); // Contact page
 
-Route::get('/search-notice', [NoticeController::class, 'searchNotices'])->name('search-notice');
+// Rutas de anuncios
+Route::get('/upload-notice', [NoticeController::class, 'uploadNotice'])->name('upload-notice.index'); // Upload notice form
+Route::post('/upload-notice', [NoticeController::class, 'store'])->name('upload-notice.store'); // Store uploaded notice
+Route::get('/my-notices', [MyNoticesController::class, 'index'])->name('my-notices.index'); // User's own notices
+Route::get('/notices', [NoticeController::class, 'getAllNotices'])->middleware(CheckIfUserIsAdmin::class)->name('get-all-notices'); // Admin view all notices
+Route::get('/category/{id}', [CategoryController::class, 'showCategory'])->name('category.show'); // Show specific category
+Route::get('/notice/{id}', [NoticeController::class, 'showNotice'])->name('notice.show'); // View a specific notice
+Route::get('/notice-modify/{id}', [NoticeController::class, 'modifyNotice'])->name('notice.modify'); // Modify notice
+Route::post('/notice-modify/{id}', [NoticeController::class, 'storeModifiedNotice'])->name('notice.update'); // Store modified notice
+Route::get('/search-notice', [NoticeController::class, 'searchNotices'])->name('search-notice'); // Search for notices
 
-Route::post('/notice-modify/{id}/up-to-gallery', [NoticeController::class, 'storeModifiedNotice'])->name('up-to-gallery.post');
+// Rutas de destacados
+Route::post('/notice-modify/{id}/up-to-gallery', [NoticeController::class, 'storeModifiedNotice'])->name('up-to-gallery.post'); // Store in gallery
 Route::get('/notice-modify/{id}/up-to-gallery', function ($id) {
     return view('up-to-gallery.index', ['notice_id' => $id]);
-})->middleware(CheckIfNoticeIsHighlighted::class)->name('up-to-gallery.get');
+})->middleware(CheckIfNoticeIsHighlighted::class)->name('up-to-gallery.get'); // View highlighted notice form
 
-Route::get('/users', [DashboardController::class, 'showUsers'])->middleware(CheckIfUserIsAdmin::class)->name('get-users');
+// Ruta de manejo de usuarios
+Route::get('/users', [DashboardController::class, 'showUsers'])->middleware(CheckIfUserIsAdmin::class)->name('get-users'); // Admin view users
+Route::get('/account', [AccountController::class, 'index'])->name('account.index');
+Route::put('/account', [AccountController::class, 'modifyUserInformation'])->name('account.post');
 
+// Ruta de busqueda
+Route::get("search", [NoticeController::class, 'search'])->name('search'); // Global search
 
-// search route
+// Ruta de dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard'); // User dashboard
 
-Route::get("search", [NoticeController::class, 'search'])->name('search');
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
+// Ruta de autenticacion
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit'); // Edit profile
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update'); // Update profile
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); // Delete profile
 });
+
 
 require __DIR__ . '/auth.php';
